616ef4040e52be7db8c049162eab9627f996acd8
Carl Fee <cfee@localhost.localdomain>
2017-10-11 00:24:57 -0600

    new-code-move-archon

--- src/team012/RoamingArchon.java
+++ src/team012/RoamingArchon.java
@@ -1,11 +1,9 @@
 package team012;
 
-import battlecode.common.Direction;
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
-import battlecode.common.RobotType;
+import battlecode.common.*;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class RoamingArchon extends Bot{
 
@@ -15,6 +13,9 @@ public class RoamingArchon extends Bot{
     static int archonId = 0;
     static boolean isFirstA = false;
     static RobotType typeToBuild = RobotType.SCOUT;
+    static MapLocation spot = rc.getInitialArchonLocations(myTeam)[0];
+    static int lastTimeD = 0;
+    static Random rand = new Random(rc.getID());
 
     public RoamingArchon(){
 
@@ -29,10 +30,89 @@ public class RoamingArchon extends Bot{
 
     public void runFrame() throws GameActionException{
 
-
-
-
     }
+
+    /*private void roamAway(){
+        if (rc.isCoreReady()) {
+            //Outside of safe areas
+            int minD = 50;
+
+            MapLocation now = rc.getLocation();
+            int xD = Math.abs(spot.x - now.x);
+            int yD = Math.abs(spot.y - now.y);
+            int distance = (int)Math.sqrt(xD*xD + yD*yD);
+            minD = distance;
+
+            boolean movingAway = (minD >= lastTimeD);
+            boolean outOfCircles = (minD > 15);
+            lastTimeD = minD;
+            RobotInfo[] infoE = rc.senseNearbyRobots(10,enemyTeam);
+            RobotInfo[] infoZ = rc.senseNearbyRobots(10,enemyTeam);
+
+            if(rc.canMove(current) &&(outOfCircles || movingAway)) {
+            } else {
+                current = directions[(rand.nextInt(8))];
+                for(int i = 0; i < 8;i++){
+                    if(rc.canMove(current)){
+                        break;
+                    }
+                    current.rotateLeft();
+                }
+            }
+            // NearbyZombies
+            if(infoZ.length != 0){
+                //Nearby Enemy, then stop (never calls move command)
+                if(infoE.length == 0){
+                    boolean moved = false;
+                    Direction curLeft = current.rotateLeft();
+                    Direction current2 = current;
+                    for(int i = 0; i < 4; i++){
+                        boolean leftC = true;
+                        int lX = curLeft.dx;
+                        int lY = curLeft.dy;
+                        boolean currentC = true;
+                        int cX = current2.dx;
+                        int cY = current2.dy;
+                        // Check that extended line does not hit any zombies
+                        // May want it to check different ranges at some point
+                        for(int j = 0; j < infoZ.length; j++){
+                            //if current is bad
+                            RobotInfo z = infoZ[i];
+                            int changeX = z.location.x - rc.getLocation().x;
+                            int changeY = z.location.y - rc.getLocation().y;
+                            if(cX == Math.abs(changeX)/changeX || cY == Math.abs(changeY)/changeY){
+                                currentC = false;
+                            }
+                            if(lX == Math.abs(changeX)/changeX || lY == Math.abs(changeY)/changeY){
+                                leftC = false;
+                            }
+                        }
+                        if(currentC && rc.canMove(current2)){
+                            rc.move(current2);
+                            moved = true;
+                            break;
+                        } else if(leftC && rc.canMove(curLeft)){
+                            rc.move(curLeft);
+                            moved = true;
+                            break;
+                        }
+                        current2.rotateRight();
+                        curLeft.rotateLeft();
+                    }
+                    if(!moved){
+                        rc.move(current);
+                    }
+                }
+            } else{
+                if(infoE.length == 0) current.opposite();
+                rc.move(current);
+            }
+
+        }
+
+
+
+    }*/
 
     public boolean buildFromList(Direction dir) throws GameActionException {
         if (buildList.isEmpty() == false) {
