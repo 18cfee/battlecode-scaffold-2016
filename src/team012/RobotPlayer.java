d6d0157c98b67eaf281526d88bebc26939a4c546
gamerpuppy <keegan.millard@gmail.com>
2017-10-13 23:21:15 -0600

    removed instance creation

--- src/team012/RobotPlayer.java
+++ src/team012/RobotPlayer.java
@@ -1,5 +1,6 @@
 package team012;
 
+import battlecode.common.Clock;
 import battlecode.common.RobotController;
 
 public class RobotPlayer {
@@ -11,38 +12,32 @@ public class RobotPlayer {
     public static RobotController rc;
 
     public static void run(RobotController rc) {
+
         RobotPlayer.rc = rc;
 
         Global.init();
-        Global bot;
         switch (rc.getType()) {
             case ARCHON:
-                bot = new Archon();
                 Archon.init();
                 Archon.loop();
                 break;
             case SOLDIER:
-                bot = new Soldier();
                 Soldier.init();
                 Soldier.loop();
                 break;
             case SCOUT:
-                bot = new Scout();
                 Scout.init();
                 Scout.loop();
                 break;
             case TURRET:
-                bot = new Turret();
                 Turret.init();
                 Turret.loop();
                 break;
             case GUARD:
-                bot = new Guard();
                 Guard.init();
                 Guard.loop();
                 break;
             default:
-                bot = new DefaultBot();
                 DefaultBot.init();
                 DefaultBot.loop();
                 break;
