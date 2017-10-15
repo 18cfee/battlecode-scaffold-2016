1d60d343dabb7e3c0665ab553c686f5239679c01
Jase Rost <jaserost@yahoo.com>
2017-10-14 18:50:08 -0600

    Trying to get the guards to patrol the turrets

--- src/team012/Guard.java
+++ src/team012/Guard.java
@@ -9,21 +9,274 @@ public class Guard extends Global {
     }
 
     private static Direction moving = Direction.NORTH;
+    private static MapLocation oldLoc = myLoc;
+    private static boolean clockwise = true;
+    private static boolean cont = false;
+    private static boolean startedPatrol = false;
+    private static int turn = 1;
+
     public static void turn() throws GameActionException {
-        if(rc.isCoreReady()){
-            moving = Path.moveSomewhereOrLeft(moving);
-            if(rc.canMove(moving)){
-                rc.move(moving);
+        if(rc.isCoreReady()) {
+            if(turn == 1){
+                for (RobotInfo ally : Global.visibleAllies) {
+                    if (ally.type == RobotType.ARCHON) {
+                        if (ally.location.distanceSquaredTo(myLoc) < 4) {
+                            oldLoc = myLoc;
+                            Path.tryMoveDir(ally.location.directionTo(myLoc));
+                            moving = oldLoc.directionTo(myLoc);
+                        }
+                    }
+                }
+            }
+
+            if(startedPatrol) {
+                patrolTurrets();
+            }else{
+                for(RobotInfo ally : visibleAllies){
+                    if(ally.type == RobotType.TURRET && myLoc.directionTo(ally.location) == Direction.NORTH_WEST && myLoc.distanceSquaredTo(ally.location) < 5) {
+                        startedPatrol = true;
+                        oldLoc = myLoc;
+                        Path.tryMoveDir(Direction.WEST);
+                        moving = Direction.WEST;
+                        clockwise = true;
+                        break;
+                    }else if(ally.type == RobotType.TURRET && myLoc.directionTo(ally.location) == Direction.NORTH_EAST && myLoc.distanceSquaredTo(ally.location) < 5) {
+                        startedPatrol = true;
+                        oldLoc = myLoc;
+                        Path.tryMoveDir(Direction.EAST);
+                        moving = Direction.EAST;
+                        clockwise = false;
+                        break;
+                    }else if(ally.type == RobotType.TURRET && myLoc.directionTo(ally.location) == Direction.SOUTH_WEST && myLoc.distanceSquaredTo(ally.location) < 5){
+                        startedPatrol = true;
+                        oldLoc = myLoc;
+                        Path.tryMoveDir(Direction.WEST);
+                        moving = Direction.WEST;
+                        clockwise = false;
+                        break;
+                    }else if(ally.type == RobotType.TURRET && myLoc.directionTo(ally.location) == Direction.SOUTH_EAST && myLoc.distanceSquaredTo(ally.location) < 5) {
+                        startedPatrol = true;
+                        oldLoc = myLoc;
+                        Path.tryMoveDir(Direction.EAST);
+                        moving = Direction.EAST;
+                        clockwise = true;
+                        break;
+                    }
+                }
             }
         }
     }
 
+
+    public static void patrolTurrets() throws GameActionException{
+        cont = false;
+        Direction inMoving = moving;
+        if(clockwise){
+            if(moving == Direction.SOUTH){
+                for (RobotInfo ally : visibleAllies){
+                    if(ally.type == RobotType.TURRET && myLoc.directionTo(ally.location) == Direction.WEST && ally.location.distanceSquaredTo(myLoc) < 4){
+                        cont = true;
+                        break;
+                    }
+                }
+                if(!cont){
+                    moving = Direction.WEST;
+                }
+                if(rc.canMove(moving)){
+                    oldLoc = myLoc;
+                    Path.tryMoveDir(moving);
+                }else{
+                    clockwise = !clockwise;
+                    moving = myLoc.directionTo(oldLoc);
+                    if(rc.canMove(moving)){
+                        oldLoc = myLoc;
+                        Path.tryMoveDir(moving);
+                    }else{
+                        clockwise = !clockwise;
+                        moving = inMoving;
+                    }
+                }
+            }else if(moving == Direction.WEST) {
+                for (RobotInfo ally : visibleAllies) {
+                    if (ally.type == RobotType.TURRET && myLoc.directionTo(ally.location) == Direction.NORTH && ally.location.distanceSquaredTo(myLoc) < 4) {
+                        cont = true;
+                        break;
+                    }
+                }
+                if (!cont) {
+                    moving = Direction.NORTH;
+                }
+                if(rc.canMove(moving)){
+                    oldLoc = myLoc;
+                    Path.tryMoveDir(moving);
+                }else{
+                    clockwise = !clockwise;
+                    moving = myLoc.directionTo(oldLoc);
+                    if(rc.canMove(moving)){
+                        oldLoc = myLoc;
+                        Path.tryMoveDir(moving);
+                    }else{
+                        clockwise = !clockwise;
+                        moving = inMoving;
+                    }
+                }
+            }else if(moving == Direction.NORTH){
+                for (RobotInfo ally : visibleAllies){
+                    if(ally.type == RobotType.TURRET && myLoc.directionTo(ally.location) == Direction.EAST && ally.location.distanceSquaredTo(myLoc) < 4){
+                        cont = true;
+                        break;
+                    }
+                }
+                if(!cont){
+                    moving = Direction.EAST;
+                }
+                if(rc.canMove(moving)){
+                    oldLoc = myLoc;
+                    Path.tryMoveDir(moving);
+                }else{
+                    clockwise = !clockwise;
+                    moving = myLoc.directionTo(oldLoc);
+                    if(rc.canMove(moving)){
+                        oldLoc = myLoc;
+                        Path.tryMoveDir(moving);
+                    }else{
+                        clockwise = !clockwise;
+                        moving = inMoving;
+                    }
+                }
+            }else if(moving == Direction.EAST){
+                for (RobotInfo ally : visibleAllies){
+                    if(ally.type == RobotType.TURRET && myLoc.directionTo(ally.location) == Direction.SOUTH && ally.location.distanceSquaredTo(myLoc) < 4){
+                        cont = true;
+                        break;
+                    }
+                }
+                if(!cont){
+                    moving = Direction.SOUTH;
+                }
+                if(rc.canMove(moving)){
+                    oldLoc = myLoc;
+                    Path.tryMoveDir(moving);
+                }else{
+                    clockwise = !clockwise;
+                    moving = myLoc.directionTo(oldLoc);
+                    if(rc.canMove(moving)){
+                        oldLoc = myLoc;
+                        Path.tryMoveDir(moving);
+                    }else{
+                        clockwise = !clockwise;
+                        moving = inMoving;
+                    }
+                }
+            }
+        }else{
+            if(moving == Direction.SOUTH){
+                for (RobotInfo ally : visibleAllies){
+                    if(ally.type == RobotType.TURRET && myLoc.directionTo(ally.location) == Direction.EAST && ally.location.distanceSquaredTo(myLoc) < 4){
+                        cont = true;
+                        break;
+                    }
+                }
+                if(!cont){
+                    moving = Direction.EAST;
+                }
+                if(rc.canMove(moving)){
+                    oldLoc = myLoc;
+                    Path.tryMoveDir(moving);
+                }else{
+                    clockwise = !clockwise;
+                    moving = myLoc.directionTo(oldLoc);
+                    if(rc.canMove(moving)){
+                        oldLoc = myLoc;
+                        Path.tryMoveDir(moving);
+                    }else{
+                        clockwise = !clockwise;
+                        moving = inMoving;
+                    }
+                }
+            }else if(moving == Direction.WEST){
+                for (RobotInfo ally : visibleAllies){
+                    if(ally.type == RobotType.TURRET && myLoc.directionTo(ally.location) == Direction.SOUTH && ally.location.distanceSquaredTo(myLoc) < 4){
+                        cont = true;
+                        break;
+                    }
+                }
+                if(!cont){
+                    moving = Direction.SOUTH;
+                    System.out.println("Changing direction : " + moving.toString() + "; turn number = " + roundNum);
+                }
+                if(rc.canMove(moving)){
+                    oldLoc = myLoc;
+                    Path.tryMoveDir(moving);
+                }else{
+                    clockwise = !clockwise;
+                    moving = myLoc.directionTo(oldLoc);
+                    if(rc.canMove(moving)){
+                        oldLoc = myLoc;
+                        Path.tryMoveDir(moving);
+                    }else{
+                        clockwise = !clockwise;
+                        moving = inMoving;
+                    }
+                }
+            }else if(moving == Direction.NORTH){
+                for (RobotInfo ally : visibleAllies){
+                    if(ally.type == RobotType.TURRET && myLoc.directionTo(ally.location) == Direction.WEST && ally.location.distanceSquaredTo(myLoc) < 4){
+                        cont = true;
+                        break;
+                    }
+                }
+                if(!cont){
+                    moving = Direction.WEST;
+                }
+                if(rc.canMove(moving)){
+                    oldLoc = myLoc;
+                    Path.tryMoveDir(moving);
+                }else{
+                    clockwise = !clockwise;
+                    moving = myLoc.directionTo(oldLoc);
+                    if(rc.canMove(moving)){
+                        oldLoc = myLoc;
+                        Path.tryMoveDir(moving);
+                    }else{
+                        clockwise = !clockwise;
+                        moving = inMoving;
+                    }
+                }
+            }else if(moving == Direction.EAST){
+                for (RobotInfo ally : visibleAllies){
+                    if(ally.type == RobotType.TURRET && myLoc.directionTo(ally.location) == Direction.NORTH && ally.location.distanceSquaredTo(myLoc) < 4){
+                        cont = true;
+                        break;
+                    }
+                }
+                if(!cont){
+                    moving = Direction.NORTH;
+                }
+                if(rc.canMove(moving)){
+                    oldLoc = myLoc;
+                    Path.tryMoveDir(moving);
+                }else{
+                    clockwise = !clockwise;
+                    moving = myLoc.directionTo(oldLoc);
+                    if(rc.canMove(moving)){
+                        oldLoc = myLoc;
+                        Path.tryMoveDir(moving);
+                    }else{
+                        clockwise = !clockwise;
+                        moving = inMoving;
+                    }
+                }
+            }
+        }
+    }
     public static void loop() {
         while (true) {
             try {
                 // BEGIN TURN
                 update();
                 turn();
+                turn++;
                 // END OF TURN
             } catch(Exception e) {
                 e.printStackTrace();
